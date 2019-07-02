1409892b478d3d087f2eebf9b0f80858faef43d9
Mingjing Tang <tmjcsh@gmail.com>
2019-07-02 13:24:51 -0400

    social media button works

--- src/App.js
+++ src/App.js
@@ -5,11 +5,24 @@ import './App.css';
 import { Menu, Segment, Image, Icon} from 'semantic-ui-react'
 
 export default class extends Component {
-  state = { activeItem: 'aboutMe' }
+  state = { 
+    activeItem: 'aboutMe',
+    clickedIcon: '',
+    clicked: false
+  }
 
   handleItemClick = (e, { name }) => 
-    this.setState({ activeItem: name }
+    this.setState({ 
+      activeItem: name,
+    }
   )
+
+  handleClick = (e, {name}) => {
+    this.setState({
+      clickedIcon: name,
+      clicked: true
+    })
+  }
 
   render() {
     const { activeItem } = this.state
@@ -36,23 +49,31 @@ export default class extends Component {
           />
 
           <div className = "social">
+          <a href="https://github.com/mingjingtang"> 
             <Icon 
               size = "huge"
               name = "github square"
+              onClick={this.handleClick}
             ></Icon>
+          </a>
 
+          <a href="https://www.linkedin.com/in/mingjing/">
             <Icon
               size = "huge"
               name = "linkedin"
+              onClick={this.handleClick}
             >
             </Icon>
+          </a>
 
+          <a href="https://www.instagram.com/mingjingtang/">
             <Icon 
               size = "huge"
               name = "instagram"
+              onClick={this.handleClick}
             >
             </Icon>
-
+          </a>
 
             
           </div>
