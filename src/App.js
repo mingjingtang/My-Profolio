da84498aa2b5afd2af88e72fa9f2596157baefc1
Mingjing Tang <tmjcsh@gmail.com>
2019-07-02 12:17:08 -0400

    navBar complete

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
+
 import './App.css';
+import { Menu, Segment} from 'semantic-ui-react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class extends Component {
+  state = { activeItem: 'aboutMe' }
+
+  handleItemClick = (e, { name }) => 
+    this.setState({ activeItem: name }
+  )
+
+  render() {
+    const { activeItem } = this.state
+
+    return (
+      <div className="App">
+        <Menu tabular>
+          <Menu.Item 
+            name='aboutMe' 
+            active={activeItem === 'aboutMe'} 
+            onClick={this.handleItemClick} 
+          />
+          <Menu.Item 
+            name='myProjects' 
+            active={activeItem === 'myProjects'} 
+            onClick={this.handleItemClick} 
+          />
+        </Menu>
+
+        <Segment attached='bottom'>
+          <img src='' />
+        </Segment>
+      </div>
+    );
+  }
+
 }
-
-export default App;
