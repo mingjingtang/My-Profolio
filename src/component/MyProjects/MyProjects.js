f93e0df854d75f49734c515b48931bd60f1efda2
Mingjing Tang <tmjcsh@gmail.com>
2019-07-08 16:16:16 -0400

    add project list and link

--- src/component/MyProjects/MyProjects.js
+++ src/component/MyProjects/MyProjects.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import '../MyProjects/MyProjects.css'
+import { List } from 'semantic-ui-react'
 class MyProjects extends React.Component {
   render() {
     return (
-        <div>
-             <h1>My Projects</h1>
-        </div>
+      <div className = "projectList">
+        <h1>My Projects</h1>
+        <List divided relaxed>
+          <List.Item>
+            <List.Icon name='github' size='large' verticalAlign='middle' />
+            <List.Content>
+              <List.Header as='a' href="https://github.com/mingjingtang/Word-Search">Word Search</List.Header>
+              <List.Description as='a'>Updated 10 mins ago</List.Description>
+            </List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name='github' size='large' verticalAlign='middle' />
+            <List.Content>
+              <List.Header as='a' href="https://github.com/mingjingtang/book-club">Book Club</List.Header>
+              <List.Description as='a'>Updated 22 mins ago</List.Description>
+            </List.Content>
+          </List.Item>
+          <List.Item>
+            <List.Icon name='github' size='large' verticalAlign='middle' />
+            <List.Content>
+              <List.Header as='a' href= "https://github.com/mingjingtang/Trip-Plan">Trip Plan</List.Header>
+              <List.Description as='a'>Updated 34 mins ago</List.Description>
+            </List.Content>
+          </List.Item>
+        </List>
+      </div>
     )
   }
 }
