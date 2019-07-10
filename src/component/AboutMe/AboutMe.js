bd8bb6bbf06e76e8ac9b4f1fb09afd475823f0f1
Mingjing Tang <tmjcsh@gmail.com>
2019-07-10 11:53:31 -0400

    adjust contact color and display

--- src/component/AboutMe/AboutMe.js
+++ src/component/AboutMe/AboutMe.js
@@ -2,7 +2,7 @@ import React from 'react'
 import me from '../../image/me.jpg'
 
 import '../AboutMe/AboutMe.css'
-import { Image, Icon } from 'semantic-ui-react'
+import { Image, Icon, Header, Card } from 'semantic-ui-react'
 
 
 export default class AboutMe extends React.Component {
@@ -27,7 +27,7 @@ export default class AboutMe extends React.Component {
                 />
 
                 <div className="social">
-                    <a href="https://github.com/mingjingtang" target = "_blank">
+                    <a href="https://github.com/mingjingtang" target="_blank">
                         <Icon
                             size="large"
                             name="github square"
@@ -35,7 +35,7 @@ export default class AboutMe extends React.Component {
                         ></Icon>
                     </a>
 
-                    <a href="https://www.linkedin.com/in/mingjing/" target = "_blank">
+                    <a href="https://www.linkedin.com/in/mingjing/" target="_blank">
                         <Icon
                             size="large"
                             name="linkedin"
@@ -44,7 +44,7 @@ export default class AboutMe extends React.Component {
                         </Icon>
                     </a>
 
-                    <a href="https://www.instagram.com/mingjingtang/" target = "_blank">
+                    <a href="https://www.instagram.com/mingjingtang/" target="_blank">
                         <Icon
                             size="large"
                             name="instagram"
@@ -53,6 +53,45 @@ export default class AboutMe extends React.Component {
                         </Icon>
                     </a>
                 </div>
+
+                <div className="contactInfo">
+                    <div className="contactDetail">
+                        <Header as='h4' color='grey' className="header">
+                            ADDRESS
+                    <div class="item"></div>
+                            6020 Fresh Pond Rd
+                            Maspeth, NY, 11378
+                </Header>
+                    </div>
+
+                    <div className="contactDetail">
+                        <Header as='h4' color='grey'>
+                            PHONE
+                    <div class="item"></div>
+                            (917)690-2262
+        
+                </Header>
+                    </div>
+
+                    <div className="contactDetail">
+                        <Header as='h4' color="grey">
+                            EMAIL
+                    <div class="item"></div>
+                            tmjcsh@gmail.com
+                </Header>
+                    </div>
+
+                </div>
+
+
+                <Card className="card"
+                    link
+                    header='Mingjing Tang'
+                    meta='Software Engineer / Front End Developer'
+                    description={[
+                        'Knowledgeable Front End Developer adept at creating successful websites that meet customer needs. Collaborating with different kinds of customers to gather requirements, produce plans and improve designs for usability and functionality. Fully proficient in JavaScript, HTML and React.',
+                    ].join('')}
+                />
             </div>
         )
     }
--- src/component/MyProjects/MyProjects.js
+++ src/component/MyProjects/MyProjects.js
@@ -10,21 +10,21 @@ class MyProjects extends React.Component {
           <List.Item>
             <List.Icon name='github' size='large' verticalAlign='middle' />
             <List.Content>
-              <List.Header as='a' href="https://github.com/mingjingtang/Word-Search">Word Search</List.Header>
+              <List.Header as='a' href="https://github.com/mingjingtang/Word-Search" target = "_blank">Word Search</List.Header>
               <List.Description as='a'>Updated 10 mins ago</List.Description>
             </List.Content>
           </List.Item>
           <List.Item>
             <List.Icon name='github' size='large' verticalAlign='middle' />
             <List.Content>
-              <List.Header as='a' href="https://github.com/mingjingtang/book-club">Book Club</List.Header>
+              <List.Header as='a' href="https://github.com/mingjingtang/book-club" target = "_blank">Book Club</List.Header>
               <List.Description as='a'>Updated 22 mins ago</List.Description>
             </List.Content>
           </List.Item>
           <List.Item>
             <List.Icon name='github' size='large' verticalAlign='middle' />
             <List.Content>
-              <List.Header as='a' href= "https://github.com/mingjingtang/Trip-Plan">Trip Plan</List.Header>
+              <List.Header as='a' href= "https://github.com/mingjingtang/Trip-Plan" target = "_blank">Trip Plan</List.Header>
               <List.Description as='a'>Updated 34 mins ago</List.Description>
             </List.Content>
           </List.Item>
