ca80c646ee0da5f44da4352af2c4b4349b57de40
Mingjing Tang <tmjcsh@gmail.com>
2019-07-11 00:17:16 -0400

    add description to my projects and adjust padding

--- src/component/MyProjects/MyProjects.js
+++ src/component/MyProjects/MyProjects.js
@@ -5,27 +5,30 @@ class MyProjects extends React.Component {
   render() {
     return (
       <div className = "projectList">
-        <h3>My Projects</h3>
         <List divided relaxed>
-          <List.Item>
+          <List.Item className = "listItem">
             <List.Icon name='github' size='large' verticalAlign='middle' />
             <List.Content>
-              <List.Header as='a' href="https://github.com/mingjingtang/Word-Search" target = "_blank">Word Search</List.Header>
-              <List.Description as='a'>Updated 10 mins ago</List.Description>
+              <List.Header className = "listHeader" as='a' href="https://github.com/mingjingtang/Word-Search" target = "_blank">Word Search</List.Header>
+              <List.Description as='a'>A word search game which have two missions. Once you find all the words in the first mission,
+              you can go to second mission.</List.Description>
             </List.Content>
           </List.Item>
-          <List.Item>
+
+          <List.Item className = "listItem">
             <List.Icon name='github' size='large' verticalAlign='middle' />
             <List.Content>
-              <List.Header as='a' href="https://github.com/mingjingtang/book-club" target = "_blank">Book Club</List.Header>
-              <List.Description as='a'>Updated 22 mins ago</List.Description>
+              <List.Header className = "listHeader" as='a' href="https://github.com/mingjingtang/book-club" target = "_blank">Book Club</List.Header>
+              <List.Description as='a'>Use GoodReads API, search any book by book title. Add and delete any book you like or dislike.</List.Description>
             </List.Content>
           </List.Item>
-          <List.Item>
+
+          <List.Item className = "listItem">
             <List.Icon name='github' size='large' verticalAlign='middle' />
             <List.Content>
-              <List.Header as='a' href= "https://github.com/mingjingtang/Trip-Plan" target = "_blank">Trip Plan</List.Header>
-              <List.Description as='a'>Updated 34 mins ago</List.Description>
+              <List.Header className = "listHeader" as='a' href= "https://github.com/mingjingtang/Trip-Plan" target = "_blank">Trip Plan</List.Header>
+              <List.Description as='a'>Created all the places in the database, also user can create their own trip. Add any places they want into a specific trip,
+              or delete any places from a specific trip if they change the mind. </List.Description>
             </List.Content>
           </List.Item>
         </List>
